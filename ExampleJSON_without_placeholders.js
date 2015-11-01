d407aa1840f8bb071f9145e74f3066642dd7faf8
Mikula Beutl <mikula.beutl@gmail.com>
2015-11-01 15:16:59 +0100

    Rename ExampleJSON_with_placeholders.js to ExampleJSON_without_placeholders.js

---
+++ ExampleJSON_without_placeholders.js
@@ -0,0 +1,218 @@
+[
+  {
+   "content": "<b>Stretch marks</b> or striae (singular stria) or striae distensae, as they are called in <br />dermato with an off-color hue. They are&nbsp;...",
+   "title": "<b>Stretch marks</b> - Wikipedia, the free encyclopedia",
+   "url": "https://en.wikipedia.org/wiki/Stretch_marks",
+   "visibleUrl": "en.wikipedia.org",
+   "richSnippet": {
+    "videoobject": {
+     "url": "http://www.youtube.com/watch?v=CuSACi-VZq4",
+     "name": "HOW TO GET RID OF STRETCH MARKS!!",
+     "description": "This video is about HOW TO GET RID OF STRETCH MARKS, I shared my story on ho, it was really hard for me to share, because I fel...",
+     "paid": "False",
+     "channelid": "UC3cZMbg0LqNIRWv1pa_YyXA",
+     "videoid": "CuSACi-VZq4",
+     "duration": "PT6M50S",
+     "unlisted": "False",
+     "thumbnailurl": "http://i.ytimg.com/vi/CuSACi-VZq4/maxresdefault.jpg",
+     "embedurl": "https://www.youtube.com/embed/CuSACi-VZq4",
+     "playertype": "HTML5 Flash",
+     "width": "640",
+     "height": "360",
+     "isfamilyfriendly": "True",
+     "regionsallowed": "AD,AE,AF,AG,AI,AL,AM,AO,AQ,AR,AS,AT,AU,AW,AX,AZ,BA,BCM,CN,CO,CR,CU,CV,CW,CX,CY,CZ,DE,DJ,DK,DM,DO,DZ,EC,EE,EG,EH...",
+     "interactioncount": "183287",
+     "datepublished": "2014-09-05",
+     "genre": "Howto & Style"
+    },
+    "cseImage": {
+     "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Belly_Strech_Marks.jpg/230px-Belly_Strech_Marks.jpg",
+     "type": "1",
+     "width": "184",
+     "height": "174"
+    },
+    "metatags": {
+     "title": "Model Chrissy Teigen gave a great explanation for why she posted a photo of her stretch marks",
+     "ogTitle": "Pregnancy Stretch Marks: Types of Treatment, Prevention, and More",
+     "ogDescription": "Along with a new baby, there are the many physical changes you’lch a part of having a baby as diapers and feedings.",
+     "ogImage": "http://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/logos/webmd/web/webmd-logo-fb.jpg",
+     "ogUrl": "http://www.webmd.com/baby/features/stretch-marks",
+     "ogPublisher": "https://www.facebook.com/DocShop",
+     "ogSiteName": "WebMD",
+     "ogModifiedTime": "July 3, 2015",
+     "ogUpdatedTime": "2015-09-04T14:46:51+00:00",
+     "ogArticleModifiedTime": "2015-08-21T18:03:05-07:00",
+     "ogArticlePublishedTime": "2015-08-21T18:00:00-07:00",
+     "ogType": "article",
+     "ogVideoUrl": "https://www.youtube.com/embed/CuSACi-VZq4",
+     "ogVideoSecureUrl": "https://www.youtube.com/embed/CuSACi-VZq4",
+     "ogVideoType": "text/html",
+     "ogVideoWidth": "640",
+     "ogVideoHeight": "360",
+     "ogVideoTag": "Stretch Marks (Symptom)",
+     "ogLocale": "en_US",
+     "geoRegion": "us",
+     "fbAppId": "385978254785998",
+     "fbPageId": "111506915551297",
+     "fbAdmins": "500245829,100001836102804",
+     "articleAuthor": "https://www.facebook.com/WebMD",
+     "articlePublisher": "https://www.facebook.com/WebMD",
+     "articlePublishedTime": "2013-01-31T11:36:25+00:00",
+     "articleModifiedTime": "2015-10-16T11:34:50+00:00",
+     "articleTag": "Common Discomforts",
+     "articleSection": "Your Pregnancy",
+     "subject": "Stretch marks",
+     "searchtype": "category",
+     "acDictionary": "medlineplus-ac-dictionary",
+     "syndicationSource": "//www.docshop.com/education/dermatology/body/stretch-mark-removal",
+     "copyright": "Copyright DocShop.com 2014. All Rights Reserved.",
+     "applicationName": "Paula's Choice",
+     "msapplicationTileimage": "/_resources/www/paulaschoice/images/layout/favicon-144.png",
+     "twitterAccountId": "23603092",
+	 "twitterImageSrc": "http://www.wikihow.com/images/b/b2/Get-Rid-of-Stretch-Marks-Step-15.jpg",
+     "twitterSite": "@wikihow",
+     "twitterDomain": "docshop.com",
+     "twitterDescription": "Stretch marks can occur during suddenant women will get stretch marks around their abdomen, hips and thighs, and unfortunately, they...",
+     "twitterTitle": "How to Get Rid of Stretch Marks",
+     "twitterUrl": "http://www.wikihow.com/Get-Rid-of-Stretch-Marks",
+     "twitterAppNameIphone": "wikiHow",
+     "twitterAppIdIphone": "309209200",
+     "twitterAppUrlIphone": "wikihow://article?id=7392",
+     "twitterAppNameIpad": "YouTube",
+     "twitterAppIdIpad": "544007664",
+     "twitterAppUrlIpad": "vnd.youtube://www.youtube.com/watch?v=CuSACi-VZq4&feature=applinks",
+     "twitterAppNameGoogleplay": "YouTube",
+     "twitterCard": "summary_large_image",
+     "twitterImage": "http://static3.businessinsider.com/image/554004866da811bf71ad0cec/model-chrissy-teigen-gave-a-great-explanation-for-why-she-posted-a-photo-of-her-stretch-marks.jpg",
+     "twitterCreator": "AlyWeisman",
+     "twitterImage0": "http://media4.popsugar-assets.com/files/2015/04/15/941/n/1922153/22690641_edit_img_image_16119370_1429132915_lmain.xxxlarge.jpg",
+     "twitterImage1": "http://media4.popsugar-assets.com/files/2015/04/15/932/n/1922398/babc47e3e5fd9cca_l2npi91v.xxxlarge.jpg",
+     "appleMobileWebAppCapable": "yes",
+     "appleMobileWebAppStatusBarStyle": "black",
+     "appleMobileWebAppTitle": "Sephora",
+     "appleItunesApp": "app-id=903539455",
+     "date": "2015-04-28",
+     "publishDate": "Fri, 21 Aug 2015 18:00:00 PDT",
+     "tbiImage": "http://static3.businessinsider.com/image/554004866da811bf71ad0cec/model-chrissy-teigen-gave-a-great-explanation-for-why-she-posted-a-photo-of-her-stretch-marks.jpg",
+     "newsKeywords": "People, Chrissy Teigen, Models, Photoshop, Aly Weisman",
+     "sailthruTags": "People, Chrissy Teigen, Models, Photoshop, Aly Weisman",
+     "sailthruDescription": "\"I think we forgot what normal people look like now.\"",
+     "sailthruVerticals": "thewire",
+     "sailthruDate": "2015-04-28",
+     "sailthruTitle": "Model Chrissy Teigen gave a great explanation for why she posted a photo of her stretch marks",
+	 "sailthruImageFull": "http://static3.businessinsider.com/image/554004866da811nation-for-why-she-posted-a-photo-of-her-stretch-marks.jpg",
+	 "sailthruImageThumb": "http://static2.businessinsider.com/image/554004866da811bf71ad0cec-50-50/model-chriion-for-why-she-posted-a-photo-of-her-stretch-marks.jpg",
+     "sailthruAuthor": "Aly Weisman",
+     "author": "Aly Weisman",
+     "pmTemplate": "noad-byline",
+     "tbiVertical": "thewire",
+     "path": "treating-stretch-marks",
+     "linkedinOwner": "mid:1d5f7b",
+     "pinDescription": "#LoveYourLines Encourages Women to Embrace Their Stretch Marks",
+     "pinMedia": "http://media4.popsugar-assets.com/files/2015/04/15/941/n/1922153/2269064main.xxxlarge/i/Photos-Stretch-Marks-Love-Your-Lines.jpg",
+     "pinterestDescription": "Will anything get rid of them? Here's the skinny.",
+     "pinterestUrl": "http://www.realsimple.com/beauty-fashion/skincare/treating-stretch-marks",
+     "pinUrl": "http://www.popsugar.com/beauty/Photos-Stretch-Marks-Love-Your-Lines-37282604",
+     "alIosAppStoreId": "544007664",
+     "alIosAppName": "YouTube",
+     "alIosUrl": "vnd.youtube://www.youtube.com/watch?v=CuSACi-VZq4&feature=applinks",
+     "alAndroidUrl": "vnd.youtube://www.youtube.com/watch?v=CuSACi-VZq4&feature=applinks",
+     "alAndroidAppName": "YouTube",
+     "alAndroidPackage": "com.google.android.youtube",
+     "alWebUrl": "http://www.youtube.com/watch?v=CuSACi-VZq4&feature=applinks"
+    },
+    "article": {
+     "name": "Your Skin and Stretch Marks",
+     "description": "Find out about stretch marks, which can show up on your skin because of pregnancy, weight gain, or extreme weight loss.",
+     "articlesection": "Pregnancy",
+     "publisher": "DocShop",
+     "author": "by Erin Cullum",
+     "alternativeheadline": "Photos of Stretch Marks | Love Your Lines",
+     "headline": "#LoveYourLines Encourages Women to Embrace Their Stretch Marks",
+     "audience": "Patient",
+     "datemodified": "July 3, 2015",
+     "datepublished": "2015-08-21T18:00:00-07:00",
+     "title": "Pregnancy",
+     "genre": "How-to",
+     "image": "http://assets.babycenter.com/ims/2014/10/482650189_wide.jpg?width=475",
+     "articlebody": "What are stretch marks?Stretch marks are small, depressed streaks in the of pregnancy when the belly is rapidly expanding to accommoda..."
+    },
+    "organization": {
+     "url": "http://www.medicinenet.com/script/main/hp.asp",
+     "logo": "http://images.medicinenet.com/images/medicinenet/header/medicinenet_logo.png",
+     "brand": "Mama Mio"
+    },
+	"scraped": {  
+     "imageLink":"http://ecx.images-amazon.com/images/I/71eyUFYODRL.jpg"
+    },
+    "hcard": {  
+     "fn": "http://www.popsugar.com/author/Erin-Cullum"
+    },
+    "webpage": [
+     {
+      "breadcrumb": "home \u003e skin center \u003e skin a-z list \u003e stretch marks index \u003e stretch marks article"
+     },
+     {
+      "lastreviewed": "8/29/2014"
+     }
+    ],
+    "breadcrumb": [
+     {
+      "title": "Home"
+     },
+     {
+      "title": "Beauty & Style During Pregnancy"
+     }
+    ],
+    "creativework": [
+     {
+      "author": "Gary W. Cole, MD, FAAD",
+      "editor": "Melissa Conrad Stöppler, MD"
+     },
+     {
+      "alternativeheadline": "Stretch mark facts"
+     }
+    ],
+    "product": {
+     "name": "Mama Mio Goodbye Stretch Marks Visible Stretch Mark Minimiser (3.4 fl oz)",
+     "description": "Pregnancy experts Mama Mio present the Goodbye Stretch Marks Stretch Markn skin. Five powerful ingredients..."
+    },
+    "person": [
+     {
+      "name": "Vivianww"
+	 }
+    ],
+    "offer": {
+     "pricecurrency": "USD",
+     "price": "$60.00",
+     "availability": "InStock"
+    },
+    "review": [
+     {
+      "reviewer": "Vivianww",
+      "reviewdate": "2015-01-10",
+      "ratingstars": "5.0"
+     },
+     {
+      "name": "Works if use 3 times a day",
+      "description": "I like mio's smell, it's like citrus but more sophisticated than that. The s and they are getting better now....",
+      "datepublished": "Sat Jan 10 02:47:00 GMT 2015"
+     },
+     {  
+	   "url":"Skin Pen on Old Pregnancy Stretch Marks - Westlake Village, CA",
+	   "author":"Jlmk123",
+	   "image":"//fi.realself.com/tiny/de23a89c6dca5031ec57e8431c0e0422/4/1/3/userimage-1118240.jpg"
+	 }
+    ],
+    "comment": [
+     {
+      "author": "Richard Carbonell",
+      "datecreated": "2015-04-16T04:51:23.000Z",
+      "text": "And so fddfg  fdg dfgd gd"
+     }
+    ],
+    "item": {
+     "title": "The Truth About Stretch Marks"
+    }
+  }
+]
