161365874e25d431b6faedbb9d4de08232bae721
Mikula Beutl <mikula.beutl@gmail.com>
2015-11-01 15:16:31 +0100

    Create ExampleJSON_with_placeholders.js

---
+++ ExampleJSON_with_placeholders.js
@@ -0,0 +1,218 @@
+[
+  {
+   "content": "%content%",
+   "title": "%title%",
+   "url": "%url%",
+   "visibleUrl": "%visibleUrl%",
+   "richSnippet": {
+	"videoobject": {
+     "url": "%videoobjecturl%",
+     "name": "%videoobjectname%",
+     "description": "%videoobjectdescription%",
+     "paid": "%videoobjectpaid%",
+     "channelid": "%videoobjectchannelid%",
+     "videoid": "%videoobjectvideoid%",
+     "duration": "%videoobjectduration%",
+     "unlisted": "%videoobjectunlisted%",
+     "thumbnailurl": "%videoobjectthumbnailurl%",
+     "embedurl": "%videoobjectembedurl%",
+     "playertype": "%videoobjectplayertype%",
+     "width": "%videoobjectwidth%",
+     "height": "%videoobjectheight%",
+     "isfamilyfriendly": "%videoobjectisfamilyfriendly%",
+     "regionsallowed": "%videoobjectregionsallowed%",
+     "interactioncount": "%videoobjectinteractioncount%",
+     "datepublished": "%videoobjectdatepublished%",
+     "genre": "%videoobjectgenre%"
+    },
+    "cseImage": {
+     "src": "%cseImagesrc%",
+     "type": "%cseImagetype%",
+     "width": "%cseImagewidth%",
+     "height": "%cseImageheight%"
+    },
+    "metatags": {
+     "title": "%metatagstitle%",
+	 "ogTitle": "%metatagsogTitle%",
+	 "ogDescription": "%metatagsogDescription%",
+	 "ogImage": "%metatagsogImage%",
+	 "ogUrl": "%metatagsogUrl%",
+     "ogPublisher": "%metatagsogPublisher%",
+	 "ogSiteName": "%metatagsogSiteName%",
+     "ogModifiedTime": "%metatagsogModifiedTime%",
+     "ogUpdatedTime": "%metatagsogUpdatedTime%",
+     "ogArticleModifiedTime": "%metatagsogArticleModifiedTime%",
+     "ogArticlePublishedTime": "%metatagsogArticlePublishedTime%",
+     "ogType": "%metatagsogType%",
+	 "ogVideoUrl": "%metatagsogVideoUrl%",
+     "ogVideoSecureUrl": "%metatagsogVideoSecureUrl%",
+     "ogVideoType": "%metatagsogVideoType%",
+     "ogVideoWidth": "%metatagsogVideoWidth%",
+     "ogVideoHeight": "%metatagsogVideoHeight%",
+     "ogVideoTag": "%metatagsogVideoTag%",
+     "ogLocale": "%metatagsogLocale%",
+     "geoRegion": "%metatagsgeoRegion%",
+     "author": "%metatagsauthor%",
+     "articleAuthor": "%metatagsarticleAuthor%",
+     "articlePublisher": "%metatagsarticlePublisher%",
+     "articlePublishedTime": "%metatagsarticlePublishedTime%",
+     "articleModifiedTime": "%metatagsarticleModifiedTime%",
+     "articleTag": "%metatagsarticleTag%",
+     "articleSection": "%metatagsarticleSection%",
+     "subject": "%metatagssubject%",
+     "copyright": "%metatagscopyright%",
+     "date": "%metatagsdate%",
+     "publishDate": "%metatagspublishDate%",
+     "applicationName": "%metatagsapplicationName%",
+     "linkedinOwner": "%metatagslinkedinOwner%",
+     "searchtype": "%metatagssearchtype%",
+     "acDictionary": "%metatagsacDictionary%",
+     "syndicationSource": "%metatagssyndicationSource%",
+     "twitterAccountId": "%metatagstwitterAccountId%",
+	 "twitterImageSrc": "%metatagstwitterImageSrc%",
+     "twitterSite": "%metatagstwitterSite%",
+     "twitterDomain": "%metatagstwitterDomain%",
+     "twitterDescription": "%metatagstwitterDescription%",
+     "twitterTitle": "%metatagstwitterTitle%",
+     "twitterUrl": "%metatagstwitterUrl%",
+     "twitterAppNameIphone": "%metatagstwitterAppNameIphone%",
+     "twitterAppIdIphone": "%metatagstwitterAppIdIphone%",
+     "twitterAppUrlIphone": "%metatagstwitterAppUrlIphone%",
+     "twitterAppNameIpad": "%metatagstwitterAppNameIpad%",
+     "twitterAppIdIpad": "%metatagstwitterAppIdIpad%",
+     "twitterAppUrlIpad": "%metatagstwitterAppUrlIpad%",
+     "twitterAppNameGoogleplay": "%metatagstwitterAppNameGoogleplay%",
+     "twitterCard": "%metatagstwitterCard%",
+     "twitterImage": "%metatagstwitterImage%",
+     "twitterCreator": "%metatagstwitterCreator%",
+     "twitterImage0": "%metatagstwitterImage0%",
+     "twitterImage1": "%metatagstwitterImage1%",
+     "tbiImage": "%metatagstbiImage%",
+     "msapplicationTileimage": "%metatagsmsapplicationTileimage%",
+     "newsKeywords": "%metatagsnewsKeywords%",
+     "sailthruTags": "%metatagssailthruTags%",
+     "sailthruDescription": "%metatagssailthruDescription%",
+     "sailthruVerticals": "%metatagssailthruVerticals%",
+     "sailthruDate": "%metatagssailthruDate%",
+     "sailthruTitle": "%metatagssailthruTitle%",
+     "sailthruImageFull": "%metatagssailthruImageFull%",
+     "sailthruImageThumb": "%metatagssailthruImageThumb%",
+     "sailthruAuthor": "%metatagssailthruAuthor%",
+     "pmTemplate": "%metatagspmTemplate%",
+     "tbiVertical": "%metatagstbiVertical%",
+     "path": "%metatagspath%",
+     "pinDescription": "%metatagspinDescription%",
+     "pinMedia": "%metatagspinMedia%",
+     "pinterestDescription": "%metatagspinterestDescription%",
+     "pinterestUrl": "%metatagspinterestUrl%",
+     "pinUrl": "%metatagspinUrl%",
+     "alWebUrl": "%metatagsalWebUrl%",
+     "alIosAppStoreId": "%metatagsalIosAppStoreId%",
+     "alIosAppName": "%metatagsalIosAppName%",
+     "alIosUrl": "%metatagsalIosUrl%",
+     "alAndroidUrl": "%metatagsalAndroidUrl%",
+     "alAndroidAppName": "%metatagsalAndroidAppName%",
+     "alAndroidPackage": "%metatagsalAndroidPackage%",
+     "appleMobileWebAppCapable": "%metatagsappleMobileWebAppCapable%",
+     "appleMobileWebAppStatusBarStyle": "%metatagsappleMobileWebAppStatusBarStyle%",
+     "appleMobileWebAppTitle": "%metatagsappleMobileWebAppTitle%",
+     "appleItunesApp": "%metatagsappleItunesApp%",
+     "fbAppId": "%metatagsfbAppId%",
+     "fbPageId": "%metatagsfbPageId%",
+     "fbAdmins": "%metatagsfbAdmins%",
+    },
+	"article": {
+     "name": "%articlename%",
+     "description": "%articledescription%",
+     "articlesection": "%articlearticlesection%",
+     "publisher": "%articlepublisher%",
+     "author": "%articleauthor%",
+     "alternativeheadline": "%articlealternativeheadline%",
+     "headline": "%articleheadline%",
+     "audience": "%articleaudience%",
+     "datemodified": "%articledatemodified%",
+     "datepublished": "%articledatepublished%",
+     "title": "%articletitle%",
+     "genre": "%articlegenre%",
+     "image": "%articleimage%",
+     "articlebody": "%articlearticlebody%"
+    },
+    "organization": {
+     "url": "%organizationurl%",
+     "logo": "%organizationlogo%",
+     "brand": "%organizationbrand%"
+    },
+	"scraped": {  
+     "imageLink": "%scrapedimageLink%"
+    },
+    "hcard": {  
+     "fn": "%hcardfn%"
+    },
+    "webpage": [
+     {
+      "breadcrumb": "%webpagebreadcrumb%"
+     },
+     {
+      "lastreviewed": "%webpagelastreviewed%"
+     }
+    ],
+    "breadcrumb": [
+     {
+      "title": "%breadcrumbtitle%"
+     },
+     {
+      "title": "%breadcrumbtitle%"
+     }
+    ],
+    "creativework": [
+     {
+      "author": "%creativeworkauthor%",
+      "editor": "%creativeworkeditor%"
+     },
+     {
+      "alternativeheadline": "%creativeworkalternativeheadline%"
+     }
+    ],
+    "product": {
+     "name": "%productname%",
+     "description": "%productdescription%"
+    },
+    "person": [
+     {
+      "name": "%personname%"
+	 }
+    ],
+    "offer": {
+     "pricecurrency": "%offerpricecurrency%",
+     "price": "%offerprice%",
+     "availability": "%offeravailability%"
+    },
+    "review": [
+     {
+      "reviewer": "%reviewreviewer%",
+      "reviewdate": "%reviewreviewdate%",
+      "ratingstars": "%reviewratingstars%"
+     },
+     {
+      "name": "%reviewname%",
+      "description": "%reviewdescription%",
+      "datepublished": "%reviewdatepublished%"
+     },
+     {  
+	   "url": "%reviewurl%",
+	   "author": "%reviewauthor%",
+	   "image": "%reviewimage%"
+	 }
+    ],
+    "comment": [
+     {
+      "author": "%commentauthor%",
+      "datecreated": "%commentdatecreated%",
+      "text": "%commenttext%"
+     }
+    ],
+    "item": {
+     "title": "%itemtitle%"
+    }
+  }
+]
