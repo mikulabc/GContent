1170def726910e0e4e1b0ee8c857698e505b7624
Mikula Beutl <mikula.beutl@gmail.com>
2015-11-01 15:14:25 +0100

    Create new.php

---
+++ new.php
@@ -0,0 +1,20 @@
+<?php
+	include("xml_to_html.php");
+?>
+<?php
+	echo '<item>
+		<title>'.$_GET['keywords'].'</title>
+		<dc:creator><![CDATA[mikulabc]]></dc:creator>
+		<content:encoded><![CDATA[
+	
+	';
+?>
+  
+
+
+<?php
+getThis();
+?>
+
+
+  
